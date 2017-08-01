8d5ec503a372981cb243ffda9bd4cf182b9479fe
LordHagen <olehag04@nfk.no>
2017-08-01 13:31:56 +0200

    PS-Script

---
+++ Outlook Delegate Waste and Sent Office13.ps1
@@ -0,0 +1,10 @@
+#Lord Hagen / olehag04@nfk.no
+$RegKeyWaste = "HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook\Prefrences"
+New-Item -Path $RegKeyWaste -Name DelegateSentItemsStyle -Force
+New-ItemProperty -Path $RegKeyWaste -Name DelegateSentItemsStyle -Value 1
+Get-ItemProperty $RegKeyWaste
+
+$RegKeySent = "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\General"
+New-Item -Path $RegKeySent -Name DelegateWastebasketStyle -Force
+New-ItemProperty -Path $RegKeySent -Name DelegateWastebasketStyle -Value 4
+Get-ItemProperty $RegKeySent
